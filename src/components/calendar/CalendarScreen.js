399f3b24426050ec7ed064bc6ab733a65f0529c0
HazaelLopez <hazael@HazaelLopez.HazaelLopez.edu>
2021-02-22 09:27:55 -0600

    React Big Calendar

--- src/components/calendar/CalendarScreen.js
+++ src/components/calendar/CalendarScreen.js
@@ -1,11 +1,30 @@
 import React from 'react';
 import Navbar from '../ui/Navbar';
+import { Calendar, momentLocalizer } from 'react-big-calendar';
+import moment from 'moment';
+
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+
+const localizer = momentLocalizer(moment) // or globalizeLocalizer
+
+const events = [{
+    title: "Cumpleaños De Adriana",
+    start: moment().toDate(),// New Data en moment
+    end: moment().add(2, 'hours').toDate(),
+    bgcolor: '#fafafa'
+}];
 
 const CalendarScreen = () => {
     return (
         <React.Fragment>
-            <div>
+            <div className="calendar-screen">
                 <Navbar/>
+                <Calendar
+                    localizer={localizer}
+                    events={events}
+                    startAccessor="start"
+                    endAccessor="end"
+                />
             </div>
         </React.Fragment>
     )
