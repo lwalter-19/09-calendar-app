45dccd8ce3250e373f82611deecd2acdf4b20103
HazaelLopez <hazael@HazaelLopez.HazaelLopez.edu>
2021-02-24 22:38:47 -0600

    AuthReducer

---
+++ src/reducers/authReducer.js
@@ -0,0 +1,18 @@
+
+const initialState = {
+    checking: true,
+    //uid: null,
+    //name: null
+}
+
+export const authReducer = ( state = initialState, action ) => {
+
+    switch (action.type) {
+
+        
+        
+        default:
+            return state;
+    }
+}
+
--- src/types/types.js
+++ src/types/types.js
@@ -9,4 +9,12 @@ export const types = {
     eventClearActiveEvent: '[Event] Clear active event',
     eventUpdated: '[Event] Event updated',
     eventDeleted: '[Event] Event deleted',
+
+    authChecking: '[Auth] Checking login state',
+    authcheckingFinish: '[Auth] Finish checking login state',
+    authStartLogin: '[Auth] Start login',
+    authLogin: '[Auth] Login',
+    authStartRegister: '[Auth] Start Register',
+    authStartTokenRenew: '[Auth] Start token renew',
+    authLogout: '[Auth] Logout'
 }
