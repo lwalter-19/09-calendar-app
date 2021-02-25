ed2307c3ec1c767fd6b11aab0edcdee928bf0b50
HazaelLopez <hazael@HazaelLopez.HazaelLopez.edu>
2021-02-24 22:53:50 -0600

    Disparar la acción de inicio del login de usuario

--- src/components/auth/LoginScreen.js
+++ src/components/auth/LoginScreen.js
@@ -1,19 +1,43 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { startLogin } from '../../actions/auth';
+import { useForm } from '../../hooks/useForm';
 import './login.css';
 
 const LoginScreen = () => {
+
+    const dispatch = useDispatch();
+
+    const [ formLoginValues, handleLoginInputChange ] = useForm({
+        lEmail: "walter134@gmail.com",
+        lPassword: "123456"
+    });
+
+    const { lEmail, lPassword } = formLoginValues;
+
+    const handleLogin = (ev) => {
+        ev.preventDefault();
+        dispatch(startLogin(lEmail, lPassword));
+    }
+
+
     return (
         <React.Fragment>
             <div className="container login-container">
                 <div className="row">
                     <div className="col-md-6 login-form-1">
                         <h3>Ingreso</h3>
-                        <form>
+                        <form
+                            onSubmit={ handleLogin }
+                        >
                             <div className="form-group">
                                 <input 
                                     type="text"
                                     className="form-control"
                                     placeholder="Correo"
+                                    name="lEmail"
+                                    value={ lEmail }
+                                    onChange={ handleLoginInputChange }
                                 />
                             </div>
                             <div className="form-group">
@@ -21,6 +45,9 @@ const LoginScreen = () => {
                                     type="password"
                                     className="form-control"
                                     placeholder="Contraseña"
+                                    name="lPassword"
+                                    value={ lPassword }
+                                    onChange={ handleLoginInputChange }
                                 />
                             </div>
                             <div className="form-group">
